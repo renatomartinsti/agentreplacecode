66e23bc4ff5bbadb48a9d0d8affe2459f03b0dff
Renato Martins <renatomartinsti@gmail.com>
2017-12-28 21:04:34 -0200

    add unit tests

--- src/test/java/com/github/remartins/bytechameleon/XStreamTest.java
+++ src/test/java/com/github/remartins/bytechameleon/XStreamTest.java
@@ -1,8 +1,10 @@
 package com.github.remartins.bytechameleon;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.util.ArrayList;
 
 import org.junit.Test;
 
@@ -38,6 +40,60 @@ public class XStreamTest {
 		for (int i = 1; i < real.length; i++) {
 			assertTrue(real[i].equals(result[i]));
 		}
+	}
+	
+	
+	@Test
+	public void writeXml() {
+		
+		Method method = new Method();
+		method.setName("message");
+		method.setParams("String");
+		method.setType("before");
+		method.setCode("System.out.println(\"Hello before call !!!\");");
+		
+		Clazz clazz = new Clazz();
+		clazz.setName("Messenger");
+		clazz.setMethods(new ArrayList<Method>());
+		clazz.getMethods().add(method);
 
+		ByteChameleon bc = new ByteChameleon();
+		bc.setClasses(new ArrayList<Clazz>());
+		bc.getClasses().add(clazz);
+		
+		XStream xStream = new XStream();
+		Class<?>[] classes = new Class[] { ByteChameleon.class, Clazz.class, Method.class };
+		XStream.setupDefaultSecurity(xStream);
+		xStream.allowTypes(classes);
+		xStream.processAnnotations(ByteChameleon.class);
+		
+		String result = xStream.toXML(bc).replaceAll("&quot;", "\"");
+		String[] resultArray = result.split("\n");
+		
+		
+		String[] padrao = {
+				"<byte-chameleon>",
+				"  <classes>",
+				"    <class>",
+				"      <name>Messenger</name>",
+				"      <methods>",
+				"        <method type=\"before\">",
+				"          <name>message</name>",
+				"          <params>String</params>",
+				"          <code>System.out.println(\"Hello before call !!!\");</code>",
+				"        </method>",
+				"      </methods>",
+				"    </class>",
+				"  </classes>",
+				"</byte-chameleon>"
+		};
+		
+		assertEquals(padrao.length, resultArray.length);
+		
+		for (int i = 0; i < padrao.length - 1; i++) {
+			assertTrue("Linha " + (i + 1) + ": " + resultArray[i], resultArray[i].equals(padrao[i]));
+		}
+		
+		
 	}
 }
